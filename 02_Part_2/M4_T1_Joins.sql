1379f4d2df6ecaf64bfb021e97166489ad6559b8
GitHub Desktop RGR <bascular-rendiment0x@icloud.com>
2026-01-19 22:25:10 +0100

    Add SQL exercise for joins in M4_T1_Joins.sql

---
+++ 02_Part_2/M4_T1_Joins.sql
@@ -0,0 +1,17 @@
+/* 
+1.Mostra el nom del llibre i el nom de l'autor dels llibres que són d'abans del 1927.
+2.Sobre la pregunta anterior, quin és l'autor amb més llibres publicats abans de 1927?
+3.Mostra el nom dels llibres i la quantitat de vegades que han estat retornats amb retard. També s'ha de mostrar la mitjana dels dies de retard.
+4.Mostra la quantitat d'usuaris que no han realitzat cap préstec.
+5.Mostra el nom dels 3 usuaris que han fet més préstecs.
+6.Mostra el nom i l'ID dels usuaris estrangers i que han hagut de pagar una multa per retard en la devolució del préstec superior a 10 euros.
+7.Mostra l'autor nascut després de 1980 que ha generat més préstecs en usuaris espanyols. A més, només s'han de comptabilitzar els préstecs finalitzats (ok o amb retard).
+8.Quina és la categoria de llibres que més demanen en préstec les persones que tenen targeta de fidelitat?
+ */ ;
+
+ -- ===============================================
+
+ -- 1.Mostra el nom del llibre i el nom de l'autor dels llibres que són d'abans del 1927.
+ ;
+
+ 
