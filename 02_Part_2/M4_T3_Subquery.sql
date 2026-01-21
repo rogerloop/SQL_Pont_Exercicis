1c8700e63a8658513fb76ce10b3385b63d816338
GitHub Desktop RGR <bascular-rendiment0x@icloud.com>
2026-01-21 20:25:03 +0100

    Update database name in settings.json and add M4_T3_Subquery.sql for subquery exercises

---
+++ 02_Part_2/M4_T3_Subquery.sql
@@ -0,0 +1,11 @@
+/* 
+TREBALLEM SOBRE LA Database Biblioteca
+
+1)Quin és el nom de l'empleat (o dels empleats) i la seva posició, amb el mínim any de contractació?
+
+2) Mostra el nom de la categoria i el nom del llibre (o llibres), dels llibres amb l'any de publicació més recent de cada categoria.
+
+3) Mostra els llibres que tenen més còpies que la mitjana del nombre de còpies dels llibres de la seva categoria.
+
+4) Quin és el nom del llibre i del seu autor, del llibre que té un import més gran en multes (comptant la suma de totes les multes de cada llibre)?
+ */
