8eeb7a1ad1ac41c0a0cc3c0c1e72d630fafd2e8b
GitHub Desktop RGR <bascular-rendiment0x@icloud.com>
2026-01-28 11:45:53 +0100

    Add SQL exercises for practicing JOINs in MP1_DDBB_hospitales_JOIN.sql and subqueries in MP2_DB_hospitales_Subquery.sql

---
+++ 03_More_practice/MP2_DB_hospitales_Subquery.sql
@@ -0,0 +1,12 @@
+/* 
+Exercicis Subqueries
+Sobre la BBDD “hospitales”, pots realitzar els següents exercicis per practicar les subqueries:
+
+» 1. Mostra el nom del hospital amb més pressupost de cada comunitat autònoma.
+
+» 2. Mostra el nom de l'hospital i el nom del pacient que te menys edat de cada hospital.
+
+» 3. Mostra els hospitals que estàn per sobre de la mitja de "indice_satisfaccion" de cada comunidad autònoma.
+ */;
+
+ 
