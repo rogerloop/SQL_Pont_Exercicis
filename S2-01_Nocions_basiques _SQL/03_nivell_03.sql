ca4de7770ac59fe1f62a10b8e4cc01926c06364d
GitHub Desktop RGR <bascular-rendiment0x@icloud.com>
2026-02-01 12:58:58 +0100

    Add SQL exercises for transaction analysis and resource allocation

---
+++ S2-01_Nocions_basiques _SQL/03_nivell_03.sql
@@ -0,0 +1,29 @@
+/* 
+Exercici 1
+
+Presenta el nom, telèfon, país, data i amount, d'aquelles empreses que van realitzar transaccions amb un valor comprès 
+entre 100 i 200 euros i en alguna d'aquestes dates: 29 d'abril del 2021, 20 de juliol del 2021 i 13 de març del 2022. 
+Ordena els resultats de major a menor quantitat.
+*/;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+Exercici 2
+
+Necessitem optimitzar l'assignació dels recursos i dependrà de la capacitat operativa que es requereixi, 
+per la qual cosa et demanen la informació sobre la quantitat de transaccions que realitzen les empreses, 
+però el departament de recursos humans és exigent i 
+vol un llistat de les empreses on especifiquis si tenen més de 4 transaccions o menys.
+*/;
